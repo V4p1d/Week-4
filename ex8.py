ac6f56c9f0ed470790004fccfaa42a8b67579183
V4p1d <p.ferraro@imperial.ac.uk>
2021-10-28 13:35:08 +0100

    Second commit

--- ex8.py
+++ ex8.py
@@ -1,2 +1,30 @@
 # Write a function that, given a list of numbers, returns a new list but with unique elements.
 # e.g., [1,1,2, 2, 2, 3, 3, 4, 5] ---> [1,2,3,4,5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+e1 = [1, 0, 0]
+e2 = [0, 1, 0]
+e3 = [0, 0, 1]
+
+M = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
